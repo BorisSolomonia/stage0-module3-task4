5255cbb316e63a907969f455a275cec8ab9e0890
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-17 21:24:07 +0400

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -6,5 +6,9 @@ public class IntRepresentation {
         int h = 'h';
         int a = 'a'; 
         int r = 'r';
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
