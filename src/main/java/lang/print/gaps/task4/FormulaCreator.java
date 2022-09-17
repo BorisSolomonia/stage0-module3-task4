b86df112f51d23e9322b34b2f80bb65a5805a983
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-17 21:20:47 +0400

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         long first = 10_000_000_000L;
-        char second = 'a';
+        int second = 'a';
         boolean third = 5 > 9;
         double forth = 12.90434323324342;
         float fifth = 12.3F;
